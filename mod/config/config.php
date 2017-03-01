1ddcefbe997b9ba01d568c43030cca34f81a4ac5
Hyurl <ayon@hyurl.com>
2017-03-01 19:27:00 +0800

    Add files via upload

--- mod/config/config.php
+++ mod/config/config.php
@@ -3,7 +3,7 @@
  * 设置常规配置
  * 模块的特殊配置项:
  * 1. {module}.template: 模板文件，相对于模板目录
- * 2. {module}.staticURL: 伪静态 URL 地址格式，关键字用花括号 {} 标识
+ * 2. {module}.staticURI: 伪静态地址格式，关键字用花括号 {} 标识
  * 3. {module}.keys.require: 新增记录时所必须提交的字段
  * 4. {module}.keys.filter: 更新记录时将会过滤的字段(不包含主键字段), 默认对管理员无效，除非在字段前加 *
  * 5. {module}.keys.serialize: 自动序列化数据的字段，在获取数据时也自动反序列化
@@ -12,11 +12,13 @@
 return array(
 	'mod' => array( //系统设置
 		'installed' => false, //是否已安装
-		'language' => 'zh_CN', //语言
+		'language' => 'zh-CN', //语言
 		'timezone' => 'Asia/Shanghai', //时区
 		'outputBuffering' => 0, //输出缓冲区大小，可设置为 1-4096(4 KB) 的值，0 则不限制，在 PHP 5.4.0 之前，1 等于 4096
 		'websocketPort' => 8080, //WebSocket 监听端口
 		'escapeTags' => '<script><style><iframe>', //过滤 HTML 标签
+		'pathinfoMode' => false, //如果开启, create_url() 创建的路径将包含模板入口文件
+		'cliCharset'=>'', //设置运行于命令行模式时操作系统所支持的编码
 		'database' => array( //数据库设置
 			'host' => 'localhost', //主机地址
 			'name' => 'modphp', //数据库名称
@@ -28,10 +30,11 @@ return array(
 		'session' => array( //Session 设置
 			'name' => 'MODID', //名称
 			'maxLifeTime' => 60*60*24*7, //生存期(分钟)
-			'savePath' => 'tmp/', //保存路径
+			'savePath' => '', //保存路径，不设置则为默认
 			),
-		'template' => array(
-			'savePath' => 'template/',
+		'template' => array( //模板设置
+			'appPath'=>'', //应用目录
+			'savePath' => 'template/', //保存目录，相对于 appPath(如果有)
 			'compiler' => array(
 				'enable' => false, //启用编译器，false/0 不启用, true/1 调试模式，每次运行都重新编译，2 产品模式，使用已有的编译结果
 				'extraTags' => array('import', 'redirect'), //额外的 HTML 语义标签
@@ -45,7 +48,7 @@ return array(
 		'URL' => '', //固定 URL 地址
 		'home' => array( //首页设置(相对于模板目录)
 			'template' => 'index.php', //模板文件
-			'staticURL' => 'page/{page}.html', //伪静态地址
+			'staticURI' => 'page/{page}.html', //伪静态地址
 			),
 		'errorPage' => array( //错误页面设置(相对于模板目录)
 			403 => '403.php', //403 页面
@@ -60,7 +63,7 @@ return array(
 		),
 	'user' => array( //用户模块设置
 		'template' => 'profile.php', //模板文件
-		'staticURL' => 'profile/{user_id}.html', //伪静态地址
+		'staticURI' => 'profile/{user_id}.html', //伪静态地址
 		'keys' => array( //字段设置
 			'login' => 'user_name|user_email|user_tel', //用户登录字段, 当设置为多个字段时，前台可统一使用 user 作为参数
 			'require' => 'user_name|user_password|user_level', //用户注册必需字段
@@ -94,7 +97,7 @@ return array(
 		),
 	'category' => array( //分类目录模块设置
 		'template' => 'category.php', //模板文件
-		'staticURL' => '{category_name}/page/{page}.html', //伪静态地址
+		'staticURI' => '{category_name}/page/{page}.html', //伪静态地址
 		'keys'=>array( //字段设置
 			'require' => 'category_name', //添加数据必需字段
 			'filter' => 'category_name', //更新数据过滤字段
@@ -102,7 +105,7 @@ return array(
 		),
 	'post' => array( //文章模块设置
 		'template' => 'single.php', //模板文件
-		'staticURL' => '{category_name}/{post_id}.html', //伪静态地址
+		'staticURI' => '{category_name}/{post_id}.html', //伪静态地址
 		'keys' => array( //字段设置
 			'require' => 'post_title|post_content|post_time|category_id|user_id', //添加数据必需字段
 			'filter' => 'post_time|user_id', //更新数据过滤字段

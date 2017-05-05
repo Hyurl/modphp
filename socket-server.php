791d15c598f84c6876393da0eba26ac17d814fe8
hyurl <ayon@hyurl.com>
2017-05-05 22:18:47 +0800

    新文件：   Start-ModPHP.bat 	新文件：   mod/classes/database.class.php 	新文件：   mod/classes/socket-server.class.php 	新文件：   mod/functions/console.func.php 	新文件：   socket-server-thread.php 	新文件：   socket-server.php 	修改：     .htaccess 	修改：     README.md 	修改：     index.php 	修改：     install.php 	修改：     mod.php 	修改：     mod/classes/category.class.php 	修改：     mod/classes/comment.class.php 	修改：     mod/classes/file.class.php 	修改：     mod/classes/image.class.php 	修改：     mod/classes/mail.class.php 	修改：     mod/classes/mod.class.php 	删除：     mod/classes/mysql.class.php 	修改：     mod/classes/post.class.php 	修改：     mod/classes/template.class.php 	修改：     mod/classes/user.class.php 	修改：     mod/classes/websocket.class.php 	修改：     mod/common/init.php 	修改：     mod/common/install.php 	修改：     mod/common/recover.php 	修改：     mod/common/update.php 	修改：     mod/config/config.php 	修改：     mod/config/database.php 	修改：     mod/functions/category.func.php 	修改：     mod/functions/comment.func.php 	修改：     mod/functions/common.func.php 	修改：     mod/functions/extension.func.php 	修改：     mod/functions/file.func.php 	修改：     mod/functions/mod.func.php 	修改：     mod/functions/post.func.php 	修改：     mod/functions/user.func.php 	修改：     mod/lang/zh-cn.php 	修改：     nginx.conf.txt 	修改：     update-log.txt 	删除：     ws.php

---
+++ socket-server.php
@@ -0,0 +1,151 @@
+<?php
+require_once('mod/common/init.php');
+/**
+ * SocketServer 说明：
+ * 直接执行 socket-server.php 将 ModPHP 运行于 Socket 服务器模式。
+ * 客户端通过发送 JSON 数据向服务器提交请求，服务器也回应以 JSON 数据。
+ * 除非是重现会话，否则 JSON 中必须包含 {obj} 和 {act} 属性，其他属性将作为请求参数。
+ * 登录用户的示例(JavaScript)：
+ * obj = {obj:'user', act:'login', user_name: 'someone', user_password: ''}
+ * WebSocket.send(JSON.stringify(obj));
+ * 重现会话的示例(JavaScript)：
+ * WebSocket.send(JSON.stringify({MODID: 'fh33v6neol7qt1r0optbspgnv6'}));
+ */
+
+/** 具体逻辑 */
+$SOCKET_INFO = $SOCKET_USER = array(); //保存连接信息的全局变量
+$WS_INFO = &$SOCKET_INFO;
+$WS_USER = &$SOCKET_USER;
+SocketServer::$maxInput = config('mod.SocketServer.maxInput'); //设置最大传入字节数
+if(is_agent()){
+	if(php_sapi_name() == 'cgi-fcgi') //Socket 服务器不能通过 FastCGI 开启
+		report_500(lang('mod.socketFastCGIWarning'));
+	if(!is_admin()) report_403();
+}
+SocketServer::on('open', function($event){ //绑定连接事件
+	global $SOCKET_INFO, $SOCKET_USER;
+	do_hooks('socket.open', $event);
+	if(isset($event['request_headers']['Cookie'])){
+		$cookie = explode_assoc($event['request_headers']['Cookie'], '; ', '=');
+		$sname = session_name();
+		if(!empty($cookie[$sname])){
+			socket_retrive_session($cookie[$sname], $event); //重现会话
+		}
+	}
+	$srcId = (int)$event['client'];
+	$SOCKET_INFO[$srcId] = array( //保存连接信息到内存中
+		'request_headers' => $event['request_headers'], //请求头
+		'session_id' => session_id(), //会话 ID
+		'user_id' => me_id() //用户 ID
+		);
+})->on('message', function($event){ //绑定消息事件
+	global $SOCKET_INFO, $SOCKET_USER, ${'DENIES'.__TIME__};
+	do_hooks('socket.message', $event);
+	if(error()) if(error()) goto sendResult;
+	$data = json_decode($event['data'], true);
+	conv_request_vars($data); //转义参数
+	$_GET['obj'] = @$data['obj'];
+	$_GET['act'] = @$data['act'];
+	$obj = strtolower($_GET['obj']);
+	$act = $_GET['act'];
+	unset($data['obj'], $data['act']);
+	$srcId = (int)$event['client'];
+	$header = $SOCKET_INFO[$srcId]['request_headers']; //取出客户端请求头
+	$hasHost = isset($header[0], $header['Host']);
+	if($hasHost) detect_site_url($header[0], $header['Host']); //检测网站地址
+	if(isset($data['HTTP_REFERER']) || $hasHost){
+		if(empty($data['HTTP_REFERER'])){
+			if($url = parse_url(site_url())){
+				extract($url);
+				$header = explode(' ', $header[0]);
+				$port = isset($port) ? ':'.$port : (is_ssl() ? ':443' : '');
+				$data['HTTP_REFERER'] = $scheme.'://'.$host.$port.$header[1];
+			}
+		}
+		$_SERVER['HTTP_REFERER'] = @$data['HTTP_REFERER'] ?: ''; //设置来路页面
+		$init = array('__DISPLAY__' => null);
+		if(config('mod.installed')){
+			do_hooks('mod.init', $init); //系统初始化接口
+			if(error()) goto sendResult;
+		}
+		$url = explode('?', $_SERVER['HTTP_REFERER']);
+		$tplPath = template_path('', false);
+		if($url[0] == site_url('mod.php')){
+			display_file($tplPath.config('site.errorPage.403'), true);
+		}elseif($init['__DISPLAY__'] === false){
+			display_file($tplPath.config('site.errorPage.404'), true);
+		}elseif($init['__DISPLAY__']){
+			display_file($init['__DISPLAY__'], true);
+		}else{
+			display_file($url[0]);
+		}
+	}
+	if(!display_file()) display_file(__SCRIPT__, true);
+	$sname = session_name();
+	if(!empty($data[$sname])){
+		if(!socket_retrive_session($data[$sname], $event)) //尝试重现会话
+			goto forbidden;
+	}elseif($sid = $SOCKET_INFO[$srcId]['session_id']){
+		session_retrieve($sid); //重现会话
+	}
+	if(!$SOCKET_INFO[$srcId]['session_id'])
+		$SOCKET_INFO[$srcId]['session_id'] = session_id();
+	if(!$SOCKET_INFO[$srcId]['user_id'])
+		$SOCKET_INFO[$srcId]['user_id'] = me_id();
+	if(($obj == 'mod' || is_subclass_of($obj, 'mod')) && (method_exists($obj, $act) || is_callable(hooks($obj.'.'.$act))) && !in_array($obj.'::'.strtolower($act), ${'DENIES'.__TIME__})){
+		$uid = me_id();
+		sendResult:
+		if(!error()) do_hooks('mod.client.call', $data);
+		$result = error() ?: $obj::$act($data);
+		$result = array_merge($result, array('obj'=>$_GET['obj'], 'act'=>$_GET['act']));
+		//调用类方法并将结果发送给客户端
+		SocketServer::send(json_encode($result)); //发送 JSON 结果
+		if($obj == 'user' && $result['success']){
+			if(!strcasecmp('login', $act)){ //登录操作
+				$uid = $result['data']['user_id'];
+				if(!isset($SOCKET_USER[$uid])) $SOCKET_USER[$uid] = array();
+				if(!in_array($event['client'], $SOCKET_USER[$uid])){
+					$SOCKET_USER[$uid][] = &$event['client']; //将用户 ID 和 socket 客户端绑定
+				}
+			}elseif(!strcasecmp('logout', $act) && $uid){ //登出
+				$i = array_search($event['client'], $SOCKET_USER[$uid]);
+				if($i !== false) unset($SOCKET_USER[$uid][$i]); //清除内存中的用户信息
+				if(!$SOCKET_USER[$uid]) unset($SOCKET_USER[$uid]);
+			}
+			$SOCKET_INFO[$srcId]['session_id'] = session_id();
+			$SOCKET_INFO[$srcId]['user_id'] = me_id();
+		}
+	}elseif(!$obj && !$act && @$data[$sname] == session_id()){
+		SocketServer::send(json_encode(user::getMe())); //重现会话操作，将登录用户的信息发送给客户端
+	}else{
+		forbidden:
+		report_403(); //未被授权的操作
+	}
+})->on('error', function($event){ //绑定错误事件
+	do_hooks('socket.error', $event);
+})->on('close', function($event){ //绑定关闭事件
+	global $SOCKET_INFO, $SOCKET_USER;
+	do_hooks('socket.close', $event);
+	$srcId = (int)$event['client'];
+	if(!empty($SOCKET_INFO[$srcId]['user_id'])){ //如果用户已登录，则清除登录信息
+		$uid = me_id() ?: $SOCKET_INFO[$srcId]['user_id'];
+		$i = array_search($event['client'], $SOCKET_USER[$uid]);
+		if($i !== false) unset($SOCKET_USER[$uid][$i]);
+		if(!$SOCKET_USER[$uid]) unset($SOCKET_USER[$uid]);
+	}
+	unset($SOCKET_INFO[$srcId]);
+});
+if(!file_exists($file = __ROOT__.'.socket-server')) file_put_contents($file, 'on');
+if(!SocketServer::server()){
+	$file = fopen($file, 'r');
+	if(!flock($file, LOCK_EX | LOCK_NB)){ //通过检测 .socket-server 文件是否被加锁来判断 Socket 服务器是否在运行
+		is_agent() ? report_500($STDOUT) : exit($STDOUT."\n");
+	}
+	//启动监听
+	SocketServer::listen(@$_SERVER['argv'][1] ?: config('mod.SocketServer.port'), function($socket){
+		$STDOUT = lang('mod.socketOnTip');
+		if($encoding = get_cmd_encoding())
+			$STDOUT = iconv('UTF-8', $encoding, $STDOUT) ?: $STDOUT;
+		if(!is_agent()) fwrite(STDOUT, $STDOUT."\n");
+	});
+}
